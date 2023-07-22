74c701281432c61aaa74bcf7c0486432912eb860
icarus0099 <rahuldevsahoo127@gmail.com>
2023-07-22 13:22:52 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,15 @@
+
+//import { WEATHER_API_URL } from './api';
+
+ export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f6974ee39dmsh5eec348f8ba3564p1628d7jsnb5fa25c40260',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "1afb37ab233696c6b7363ee4ef9f12ce";
